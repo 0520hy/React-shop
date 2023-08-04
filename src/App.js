0d765a72c04e99b52a7033bb67770ffa168965f2
연하영 <dusgkdud7@naver.com>
2023-08-05 02:51:43 +0900

    08-05

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {Navbar, Nav, Container, Row, Col} from 'react-bootstrap';
 import './App.css';
+import data from './data';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+  let [shoes] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  
+    <><div className='App'>
+
+      
+
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">AppleShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">My Page</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/detail" element={<div>상세페이지</div>} />
+        <Route path="/about" element={<div>어바웃페이지</div>} />
+      </Routes>
+
+      <div className="main-bg"></div>
+      </div>
+<Container>
+  <Row>
+    <Col className="text-center">
+      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" alt="사진1" />
+      <h4>{shoes[0].title}</h4>
+      <p>{shoes[0].price}</p>
+    </Col>
+    <Col className="text-center">
+      <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" alt="사진1" />
+      <h4>{shoes[1].title}</h4>
+      <p>{shoes[1].price}</p>
+    </Col>
+    <Col className="text-center">
+      <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" alt="사진1" />
+      <h4>{shoes[2].title}</h4>
+      <p>{shoes[2].price}</p>
+    </Col>
+  </Row>
+</Container></>
+
   );
 }
 
